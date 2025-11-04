bc822737ae065084275ab6ed74e1afff3646f7a8
Ganesh <ganeshkumarpanaganti@gmail.com>
2025-11-04 11:43:05 +0530

    Day21

---
+++ Day21/Day21.js
@@ -0,0 +1,46 @@
+// Variables
+let name = "Adventurer";
+const PI = 3.14159;
+var age = 25;
+
+// Data Types
+let text = "Hello, World!"; // String
+let num = 10; // Number
+let isActive = true; // Boolean ture/ false
+let x; // Undefined
+let person = null; // Null
+let colors = ["red", "green", "blue"]; // Array
+let car = { 
+    make: "Toyota", 
+    model: "Corolla", 
+    year: 2020 
+}; // Object
+
+// Operators
+
+// Arithmetic Operators
+let sum = 5 + 3; // Addition
+let difference = 10 - 4; // Subtraction
+let product = 6 * 7; // Multiplication
+let quotient = 20 / 5; // Division
+let remainder = 10 % 3; // Modulus
+
+// Comparison Operators
+let isEqual = (5 == '5'); // true
+let isStrictEqual = (5 === 5); // true
+let isNotEqual = (5 != 10); // true
+let isStrictNotEqual = (10 !== 5); // true
+let isGreater = (10 > 5); // true
+let isLess = (5 < 10); // true
+let isGreaterOrEqual = (10 >= 10); // true
+let isLessOrEqual = (5 <= 10); // true
+
+// Logical Operators
+let andOperator = (true && false); // false
+let orOperator = (true || false); // true
+let notOperator = !true; // false
+
+// increment and decrement
+let counter = 0;
+counter++; // Increment  counter = counter + 1
+counter--; // Decrement  counter = counter - 1
